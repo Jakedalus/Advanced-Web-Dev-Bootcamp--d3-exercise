a148d5e47191a46b66d6a99628bdcecfe7baad35
Jakedalus <jacquedalus@gmail.com>
2019-06-25 16:01:11 -0400

    Process xml data into array of objects

--- app.js
+++ app.js
@@ -1,16 +1,53 @@
+
+//d3.xml('UNdata_Export_20190625_212647973.xml', function(error, xml) {
+//	if (error) throw error;
+//	
+//	console.log(xml);
+//	console.log(xml.responseXML);
+//	console.log('what');
+//});
+
 d3.queue()
-	.defer(d3.request, './UNdata_Export_20190625_212647973.xml')
+	.defer(d3.xml, './UNdata_Export_20190625_212647973.xml')
 //	,function(row) {
 //		return {
-//			cityName: row.city,
-//			countryCode: row.iso2,
-//			CO2: +row.value
+//			row
 //		}
 //	})
 	.await(function(error, data) {
 		if (error) throw error;
 		
-		console.log(data);
+		console.log(data, typeof data);
+	
+		var records = Array.from(data.getElementsByTagName('record'));
+	
+		console.log(records, typeof records, Array.isArray(records));
+		
+		var climateData = [];
+	
+		for (var record of records) {
+//			console.log(record);
+			var fields = Array.from(record.getElementsByTagName('field'));
+//			console.log(fields);
+			
+			var recordObj = {};
+			for (var field of fields) {
+//				console.log(field.getAttribute('name'));
+//				console.log(field.textContent);
+				
+				var label = field.getAttribute('name') === 'Country or Area' ? 'country' : field.getAttribute('name').toLowerCase();
+				
+				recordObj[label] = field.textContent;
+				
+//				console.log(recordObj);
+			}
+			
+			climateData.push(recordObj);
+		}
+		
+		console.log(climateData);
+	
+		
 	
 //		var data = countries.geonames.map(country => {
 //			country.cities = cities.filter(city => city.countryCode === country.countryCode);
@@ -35,6 +72,6 @@ d3.queue()
 //			}).join(''));
 	
 	
-	})
-
-
+	});
+//
+//
